a9b3b9dac052970f96f9feaacb32c0c850677cba
Juliana Resplande <julianarsg13@gmail.com>
2023-02-03 18:41:38 -0300

    tfidf

---
+++ sentiment_analysis/utils.py
@@ -0,0 +1,77 @@
+# Based on https://github.com/nathanshartmann/portuguese_word_embeddings/blob/master/preprocessing.py
+import nltk
+import re
+
+nltk.download('punkt')
+nltk.download('rslp')
+
+word_tokenizer = re.compile(r'\s+')
+stemmer = nltk.stem.RSLPStemmer()
+
+# Punctuation list
+punctuations = re.escape('!"#%\'()*+,./:;<=>?@[\\]^_`{|}~')
+
+# ##### #
+# Regex #
+# ##### #
+re_remove_brackets = re.compile(r'\{.*\}')
+re_remove_html = re.compile(r'<(\/|\\)?.+?>', re.UNICODE)
+re_transform_numbers = re.compile(r'\d', re.UNICODE)
+re_transform_emails = re.compile(r'[^\s]+@[^\s]+', re.UNICODE)
+re_transform_url = re.compile(r'(http|https)://[^\s]+', re.UNICODE)
+# Different quotes are used.
+re_quotes_1 = re.compile(r"(?u)(^|\W)[‘’′`']", re.UNICODE)
+re_quotes_2 = re.compile(r"(?u)[‘’`′'](\W|$)", re.UNICODE)
+re_quotes_3 = re.compile(r'(?u)[‘’`′“”]', re.UNICODE)
+re_dots = re.compile(r'(?<!\.)\.\.(?!\.)', re.UNICODE)
+re_punctuation = re.compile(r'([,";:]){2},', re.UNICODE)
+re_hiphen = re.compile(r' -(?=[^\W\d_])', re.UNICODE)
+re_tree_dots = re.compile(u'…', re.UNICODE)
+# Differents punctuation patterns are used.
+re_punkts = re.compile(r'(\w+)([%s])([ %s])' %
+                       (punctuations, punctuations), re.UNICODE)
+re_punkts_b = re.compile(r'([ %s])([%s])(\w+)' %
+                         (punctuations, punctuations), re.UNICODE)
+re_punkts_c = re.compile(r'(\w+)([%s])$' % (punctuations), re.UNICODE)
+re_changehyphen = re.compile(u'–')
+re_doublequotes_1 = re.compile(r'(\"\")')
+re_doublequotes_2 = re.compile(r'(\'\')')
+re_trim = re.compile(r' +', re.UNICODE)
+
+def clean_text(text):
+    """Apply all regex above to a given string."""
+    text = text.lower()
+    text = text.replace('\xa0', ' ')
+    text = re_tree_dots.sub('...', text)
+    text = re.sub('\.\.\.', '', text)
+    text = re_remove_brackets.sub('', text)
+    text = re_changehyphen.sub('-', text)
+    text = re_remove_html.sub(' ', text)
+    text = re_transform_numbers.sub('0', text)
+    text = re_transform_url.sub('URL', text)
+    text = re_transform_emails.sub('EMAIL', text)
+    text = re_quotes_1.sub(r'\1"', text)
+    text = re_quotes_2.sub(r'"\1', text)
+    text = re_quotes_3.sub('"', text)
+    text = re.sub('"', '', text)
+    text = re_dots.sub('.', text)
+    text = re_punctuation.sub(r'\1', text)
+    text = re_hiphen.sub(' - ', text)
+    text = re_punkts.sub(r'\1 \2 \3', text)
+    text = re_punkts_b.sub(r'\1 \2 \3', text)
+    text = re_punkts_c.sub(r'\1 \2', text)
+    text = re_doublequotes_1.sub('\"', text)
+    text = re_doublequotes_2.sub('\'', text)
+    text = re_trim.sub(' ', text)
+
+    return text.strip()
+
+def preprocess_sentence(sentence: str, do_stemming: bool=False):
+    sentence = clean_text(sentence)
+
+    if do_stemming:
+        words = re.split(word_tokenizer, sentence)
+        stemmed_words = [stemmer.stem(w) for w in words if re.match(r'[a-z]', w)]
+        sentence =  " ".join(stemmed_words)
+
+    return sentence
