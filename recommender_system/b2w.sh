39374111a5db5bfa1fffc12873404eb956914149
Luana Martins <luanagbmartins@gmail.com>
2023-02-12 13:25:34 -0300

    feat: experiments scripts

--- recommender_system/b2w.sh
+++
@@ -1,9 +0,0 @@
-# Without sentiment
-python -m luigi --module recommender_system.train TrainRS --sentiment-model without --recommender-model svd --local-scheduler
-# python -m luigi --module recommender_system.train TrainRS --sentiment-model without --recommender-model svdpp --local-scheduler
-# python -m luigi --module recommender_system.train TrainRS --sentiment-model without --recommender-model nmf --local-scheduler
-
-# With Bert
-# python -m luigi --module recommender_system.train TrainRS --sentiment-model bert_timbau --recommender-model svd --local-scheduler
-# python -m luigi --module recommender_system.train TrainRS --sentiment-model bert_timbau --recommender-model svdpp --local-scheduler
-# python -m luigi --module recommender_system.train TrainRS --sentiment-model bert_timbau --recommender-model nmf --local-scheduler
